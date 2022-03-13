d7e07036dcbbd91fc966273efd6e9403c48e8ea1
粒粒橙 <i@llc.moe>
2022-03-13 09:55:45 +0000

    fix tests

--- index.test.js
+++ index.test.js
@@ -1,18 +1,18 @@
-const describeOnlyIf = (condition: boolean) =>
+const describeOnlyIf = (condition) =>
   condition ? describe.only : describe.skip
 
 describeOnlyIf(process.platform === 'win32')('unsupported platforms', () => {
   test('should throw an error', async () => {
     expect.assertions(1)
-    await expect(import('.')).rejects.toThrow(
+    await expect(require('.')).rejects.toThrow(
       `Unsupported OS: ${process.platform}-${process.arch}`,
     )
   })
 })
 
 describe('supported platforms', () => {
-  test('should not throw an error', async () => {
+  test('should not throw an error', () => {
     expect.assertions(1)
-    await expect(import('.')).resolves.toBeTruthy()
+    expect(require('.')).toBeTruthy()
   })
 })
